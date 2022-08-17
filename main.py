457a1db88b932a237f1399223036a3216f4d88cb
finan <finanmeharenakidane@gmail.com>
2022-08-17 23:17:05 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, redirect, request
+
+import json
+import requests
+import os
+
+
+app_id = os.environ.get('dictionary_api_id')
+app_key = os.environ.get('dictionary_api_key')
+endpoint = "entries"
+
+
+language_code = "en-us"
+word_id = "find"
+
+# url = "https://od-api.oxforddictionaries.com/api/v2/" + endpoint + "/" + language_code + "/" + word_id.lower()
+# response = requests.get(url, headers = {"app_id": app_id, "app_key": app_key})
+
+# print(response.json())
+
+
+app=Flask(__name__)
+
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    word=request.form.get('word')
+    url=f"https://od-api.oxforddictionaries.com/api/v2/{endpoint}/{language_code}/{word.lower()}"
+    word_definition_request=requests.get(url, headers = {"app_id": app_id, "app_key": app_key})
+    in_json=word_definition_request.json()
+    api_response=in_json['results'][0]['lexicalEntries'][0]['lexicalCategory']['text']
+
+    return api_response
+
+    # return word
+    # return render_template('result.html', word=in_json)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,20 @@
+from main import *
+
+# print(response.json())
+
+# import json
+# import requests
+# import os
+#
+# app_id  = os.environ.get('dictionary_api_id')
+# app_key = os.environ.get('dictionary_api_key')
+# endpoint = "entries"
+# language_code = "en-us"
+# word_id = "find"
+#
+#
+#
+# #
+# # print("code {}\n".format(r.status_code))
+# # print("text \n" + r.text)
+# # print("json \n" + json.dumps(r.json()))
